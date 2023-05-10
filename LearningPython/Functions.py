734b3d47325e7e27d15a95b595c78a89449f3976
Mahtoj <128434923+Mahtoj@users.noreply.github.com>
2023-05-09 17:23:33 -0700

    Practice what are functions

---
+++ LearningPython/Functions.py
@@ -0,0 +1,17 @@
+#file: Function.py
+#auth: Jotham King
+#date: 5/9/2023
+#func: Practice what are functions
+
+#example of built in functions in python
+
+print("Hello World!")
+
+#range function with 2 arguments, 2 and 20
+range(2, 20)
+
+#function str converts argument into a string
+str(12)
+
+#range function with 3 arguments, 10, 20, and 3
+range(10,20,3)
