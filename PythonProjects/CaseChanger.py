68c2497ca2767b86a81894defb4d3c74e6626fab
Mahtoj <128434923+Mahtoj@users.noreply.github.com>
2023-05-11 17:25:01 -0700

    CaseChanger

---
+++ PythonProjects/CaseChanger.py
@@ -0,0 +1,9 @@
+#file: CaseChanger.py
+#auth: Jotham King
+#date: 5/11/2023
+#func: practice ranges
+#   JK 5/1/2023 case changer program
+
+#input for user
+input("Please enter your phrase or sentence:")
+
