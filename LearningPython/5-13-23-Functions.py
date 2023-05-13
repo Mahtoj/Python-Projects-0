a8f6ac7394e18b5843424ff8d4dc4d75d501ffd3
Mahtoj <128434923+Mahtoj@users.noreply.github.com>
2023-05-13 09:43:25 -0700

    example of custom function

---
+++ LearningPython/5-13-23-Functions.py
@@ -0,0 +1,20 @@
+#file: 5-9-23-Functions.py
+#auth: Jotham King
+#date: 5/13/2023
+#func: practice creating my own functions
+
+
+
+#write a function that outputs "Welcome, [user's name]"
+user = input("What is your name?:")
+
+def welcome():
+    print("Welcome, " + user)
+
+welcome()
+
+def welcome():
+    name = input()
+    print("welcome, " + name)
+
+welcome()
