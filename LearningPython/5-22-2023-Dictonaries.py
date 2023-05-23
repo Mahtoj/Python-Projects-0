05cc3aff93bea1c7a9b0ab1fe7783fd20fb5d006
Mahtoj <128434923+Mahtoj@users.noreply.github.com>
2023-05-22 17:33:42 -0700

    short examples of dictionaires

---
+++ LearningPython/5-22-2023-Dictonaries.py
@@ -0,0 +1,23 @@
+#file: 5-22-23-Dictionaries.py
+#auth: Jotham King
+#date: 5/22/2023
+#func: examples of pythons built-in "collection types", to store multiple values
+
+
+#lists example
+x = ['hi', 'hello', 'welcome']
+#print position 3, welcome
+print(x[2])
+
+#dictonary example
+ages = {
+    "dave": 24,
+    "mary": 42,
+    "john": 58
+}
+#print key dave, value 24
+print(ages["dave"])
+#print key mary, value 42
+print(ages["mary"])
+
+
