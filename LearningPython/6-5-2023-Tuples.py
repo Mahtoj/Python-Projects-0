e1f089931d5a663bdd9f37c50edf46c74ff68c90
Mahtoj <128434923+Mahtoj@users.noreply.github.com>
2023-06-05 17:24:26 -0700

    short example of a tuple

---
+++ LearningPython/6-5-2023-Tuples.py
@@ -0,0 +1,10 @@
+#file: 6-5-23-Tuples.py
+#auth: Jotham King
+#date: 6/5/2023
+#func: examples of tuples
+
+#tuples are like list, but they are immutable (they cannot be changed)
+#tuples are created by using parentheses, rather than square brackets
+
+tuple = (1, (1, 2, 3))
+print(tuple[1])
